71644b968c7103ed6d6d5a30271503ed53eed6aa
Adi Subramanian <subramaniana@carleton.edu>
2017-07-10 20:33:54 -0400

    first commit

---
+++ jokebot.py
@@ -0,0 +1,6 @@
+#This is a reddit bot that attempts to get karma from reposts
+#It does this by finding similar posts,
+#If there are popular similar posts, it will copy the top response
+#and respond to the new post with that response
+#this is particularly meant to be used on r/jokes
+#but could potentially be used on a variety of other subreddits
